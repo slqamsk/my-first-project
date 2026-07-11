c33dc1af19847749cf556790b52d1b8cfc5b2a28
Sergey Slesarev <slqamsk@gmail.com>
2026-07-11 09:56:03 +0300

    Update MyFirstTest.java

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -5,6 +5,7 @@ import static com.codeborne.selenide.Selenide.open;
 public class MyFirstTest {
     @Test
     void test01Open() {
+        // Добавил комментарий
         open("https://slqamsk.github.io");
     }
 }
