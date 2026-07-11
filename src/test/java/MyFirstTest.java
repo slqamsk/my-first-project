54c5fabda3e39efeecf6e035b51dd958b1067db7
Sergey Slesarev <slqamsk@gmail.com>
2026-07-11 10:36:51 +0300

    maximize

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,11 +1,22 @@
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class MyFirstTest {
     @Test
     void test01Open() {
         // Добавил комментарий
-        open("https://slqamsk.github.io");
+        //Configuration.browser = "ie";
+        Configuration.browserSize = "1500x500";
+        Configuration.browserPosition = "100x200";
+
+        open("https://ru.wikipedia.org/wiki/Selenium");
+        sleep(5_000);
+        getWebDriver().manage().window().maximize();
+        sleep(5_000);
+        $("body").shouldHave(text("Selenium"));
     }
 }
