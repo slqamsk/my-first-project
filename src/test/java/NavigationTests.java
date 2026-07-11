e472e4a0712de2c941528d79445ff644621caa06
Sergey Slesarev <slqamsk@gmail.com>
2026-07-11 10:44:50 +0300

    back-forward

---
+++ src/test/java/NavigationTests.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Selenide.*;
+public class NavigationTests {
+    @Test
+    void testNavigation() {
+        open("https://ya.ru/");
+        sleep(3_000);
+        open("https://ru.wikipedia.org");
+        sleep(3_000);
+        open("https://ru.wikipedia.org/wiki/Selenium");
+        sleep(3_000);
+        back();
+        sleep(3_000);
+        back();
+        sleep(3_000);
+        forward();
+        sleep(10_000);
+    }
+}
