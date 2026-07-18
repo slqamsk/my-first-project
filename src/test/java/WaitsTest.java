74a93078bad2602ad1bbed82001b8f10f6831da2
Sergey Slesarev <slqamsk@gmail.com>
2026-07-18 14:39:23 +0300

    update Waits

---
+++ src/test/java/WaitsTest.java
@@ -0,0 +1,47 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class WaitsTest {
+    @Test
+    void test01SimpleWaitTest() {
+        open("https://slqa.ru/cases/WaitsSimpleForm/");
+        $x("//input[@id='duration_time']").setValue("10");
+        $x("//button[@id='show_element']").click();
+        Configuration.timeout = 12_000;
+        $x("//div[@id='dynamic_content']").shouldHave(text("за 10000 миллисекунд."));
+    }
+
+    @Test
+    void test02SimpleWaitsTest() {
+        open("https://slqa.ru/cases/WaitsSimpleForm/");
+        $("#duration_time").setValue("10");
+        $("#show_element").click();
+        $("#dynamic_content")
+                .shouldHave(
+                        text("за 10000 миллисекунд."),
+                        Duration.ofSeconds(12)
+                );
+        $("#wrong_id").shouldBe(exist);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
